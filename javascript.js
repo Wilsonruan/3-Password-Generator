990aac71b18b7ae0014cd7b5253b03fcc548c591
Wilson Ruan <wilsonruan23@gmail.com>
2020-05-31 02:29:52 -0400

    Renamed myFunction to myCopyFunction and create myGeneratePassword function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
-function myFunction() {
+function myCopyFunction() {
     var copyText = document.getElementById("password");
     copyText.select();
     copyText.setSelectionRange(0, 99999)
     document.execCommand("copy");
-  }
+}
+
+function myGeneratePassword(){
+    
+}
