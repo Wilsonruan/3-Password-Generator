8bdeb6b1c96ee059787c812ac95a10a2e4e8db1b
Wilson Ruan <wilsonruan23@gmail.com>
2020-05-31 02:23:46 -0400

    Initial new JavaScript.js file

--- javascript.js
+++ javascript.js
@@ -1,17 +1,3 @@
-// // Generate Password Button
-// $("#generate").on("click", function() {
-//     $("password")
-// })
-
-
-
-
-// // Copy Button
-// $("#copy").on("click", function() {
-//     $("password")
-// })
-
-// W3school experiment
 function myFunction() {
     var copyText = document.getElementById("password");
     copyText.select();
