69ca8c5d3936582780aba52986a67655ad8077a0
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-01 01:25:23 -0400

    Loop my function to exit when at least one character type is selected

--- javascript.js
+++ javascript.js
@@ -6,15 +6,24 @@ function myCopyFunction() {
 }
 
 function myGeneratePassword(){
+ 
+    let exitNow = false;
     let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-    let length = Math.floor(Math.random() * (12 - 8)) + 8;
-    let password ="";
-    for(var i = 1; i <=length; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+    let password ="";  
+
+    while (exitNow == false) {
+        let length = Math.floor(Math.random() * (12 - 8)) + 8;
+        for(var i = 1; i <=length; i++){
+            password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+        }
+        let passwordCheck = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/;
+        let result = passwordCheck.test(password);
+        exitNow = result;
+        console.log(exitNow);
+        console.log(password);
+        console.log(length);
     }
     document.getElementById("password").value = password;
-    let passwordCheck = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/;
-    let result = passwordCheck.test(password);
-    console.log(result);
-    console.log(length);
+
+    
 }
