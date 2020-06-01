cd692dc3946016b4d7f86e78f4fbd89c1ecbcd6d
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-01 01:13:00 -0400

    Add password to check Lowercase characters, Uppercase characters, Numeric characters, and Special characters

--- javascript.js
+++ javascript.js
@@ -6,14 +6,14 @@ function myCopyFunction() {
 }
 
 function myGeneratePassword(){
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()"
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
     let length = Math.floor(Math.random() * (12 - 8)) + 8;
     let password ="";
     for(var i = 1; i <=length; i++){
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
     }
     document.getElementById("password").value = password;
-    let passwordCheck = /[0-9]+/g;
+    let passwordCheck = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/;
     let result = passwordCheck.test(password);
     console.log(result);
     console.log(length);
