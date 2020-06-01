598b90f6ee7424d4c6c9d0b9f2f6483f47d839b0
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-01 00:41:57 -0400

    Add passwordCheck

--- javascript.js
+++ javascript.js
@@ -7,10 +7,14 @@ function myCopyFunction() {
 
 function myGeneratePassword(){
     let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()"
-    let length = Math.floor(Math.random() * (128 - 8)) + 8;
+    let length = Math.floor(Math.random() * (12 - 8)) + 8;
     let password ="";
-    for(var i = 0; i <=length-1; i++){
+    for(var i = 1; i <=length; i++){
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
     }
     document.getElementById("password").value = password;
+    let passwordCheck = /[0-9]+/g;
+    let result = passwordCheck.test(password);
+    console.log(result);
+    console.log(length);
 }
