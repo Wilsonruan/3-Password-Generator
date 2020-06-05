a102c4f4ed1f0c87cd6123b097588969acae760e
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-05 12:57:22 -0400

    Remove all conole.log

--- javascript.js
+++ javascript.js
@@ -18,11 +18,7 @@ function myGeneratePassword() {
         }
         let passwordCheck = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/;
         let result = passwordCheck.test(password);
-        console.log(result)
         exitNow = result;
-        console.log(exitNow);
-        console.log(password);
-        console.log(length);
     }
     document.getElementById("password").value = password;
 
